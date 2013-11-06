31d4be344f01aa10a31440d06f02c9df00020784
Marcello Gesmundo <mgesmundo@yoovant.com>
2013-11-06 09:46:09 +0100

    updated test example and doc

--- test/presstolight.js
+++ test/presstolight.js
@@ -1,17 +1,17 @@
 var daisy = require('../lib/daisy')({
-    model: 'fox',   // set to aria if you have this board
+    model: 'aria',  // set to fox if you have this board
     test: false,    // set true if you want use a fake gpio path.
                     // Note that the change, falling and rising events can't fires
     debug: false    // set true if you want see debug messages into terminal window
 });
 // create a new Daisy5 instance (8 buttons array)
-var daisy5 = new daisy.Daisy5();
+var daisy5 = new daisy.Daisy5('D12');
 // attach the init event (fired when ALL buttons are ready)
 daisy5.attach('init', function(event) {
     console.log('init daisy5');
 });
 // create a new Daisy11 instance (8 leds array)
-var daisy11 = new daisy.Daisy11();
+var daisy11 = new daisy.Daisy11('D11');
 // attach the init event (fired when ALL leds are ready)
 daisy11.attach('init', function(event) {
     console.log('init daisy11');
